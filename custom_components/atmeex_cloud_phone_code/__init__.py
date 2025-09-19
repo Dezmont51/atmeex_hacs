5cefbcc30533c4e0bd18a7ea3c9c53097aaca23e
Фокин Андрей <an.fokin@centrofinans.ru>
2025-09-19 23:06:13 +0300

    123

--- custom_components/atmeex_cloud_phone_code/__init__.py
+++ custom_components/atmeex_cloud_phone_code/__init__.py
@@ -34,12 +34,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data[DOMAIN][entry.entry_id] = coordinator
     await coordinator.async_refresh()
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(
-                entry, platform
-            )
-        )
+    # Forward entry to platforms (modern API)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 class AtmeexDataCoordinator(DataUpdateCoordinator):
